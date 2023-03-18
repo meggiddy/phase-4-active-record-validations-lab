52dfec41741adf51607ed60cf9f45285a8b476ba
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-03-18 09:21:08 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 250 }
+  validates :summary, presence: true, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+
+
+  def clickbait
+    if title.present? && !title.match(/(Won't Believe|Secret|Top \d|Guess)/)
+      errors.add(:title, "must be clickbait-y")
+    end
+  end
+
 end
